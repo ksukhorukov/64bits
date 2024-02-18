f55ec3fc5b072ecc27ae95731fb5af657e36fe7b
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2024-02-18 19:25:01 +0800

    first draft of the article with examples

---
+++ data.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+  printf("ANSI C99 data types\n");
+
+  printf("size of 'int' is: %ul bytes\n", sizeof(int));
+  printf("size of 'uint' is: %ul bytes\n", sizeof(unsigned int));
+  printf("size of 'long' is: %ul bytes\n", sizeof(long));
+  printf("size of 'long long' is: %ul bytes\n", sizeof(long long));
+  printf("size of 'long long' is: %ul bytes\n", sizeof(long long));
+}
+
